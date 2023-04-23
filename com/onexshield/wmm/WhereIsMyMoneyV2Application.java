085bea066281d5c36e18eba8bfd9f787520a9afd
Achraf Ait Ibba <97747197+achrafaitibba@users.noreply.github.com>
2023-04-23 07:03:57 +0100

    first commit

---
+++ com/onexshield/wmm/WhereIsMyMoneyV2Application.java
@@ -0,0 +1,13 @@
+package com.onexshield.wmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereIsMyMoneyV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereIsMyMoneyV2Application.class, args);
+	}
+
+}
