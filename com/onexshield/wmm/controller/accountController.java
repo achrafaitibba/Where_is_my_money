54935d0f4082a7706514026b8c433759449ed51c
Achraf Ait Ibba <97747197+achrafaitibba@users.noreply.github.com>
2023-05-20 20:28:14 +0000

    Cleaning

--- com/onexshield/wmm/controller/accountController.java
+++ com/onexshield/wmm/controller/accountController.java
@@ -62,7 +62,7 @@ public class accountController {
         return ResponseEntity.ok(accountService.updateUserInfos(request, id));
     }
 
-    @PutMapping("/update/account-infos/{id}") //todo , new email should be unique error
+    @PutMapping("/update/account-infos/{id}") 
     public ResponseEntity<accountResponse> updateAccountInfos(@RequestBody accountInfoRequest request,
                                                               @PathVariable Integer id){
         return ResponseEntity.ok(accountService.updateAccountInfos(request, id));
