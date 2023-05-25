acb841ca481fecb5cb7d5d80ec9d1ab7737fce93
Achraf Ait Ibba <97747197+achrafaitibba@users.noreply.github.com>
2023-05-25 19:30:39 +0000

    Security questions documentation done 'because there is only one endpoint'

--- com/onexshield/wmm/controller/securityQuestionController.java
+++ com/onexshield/wmm/controller/securityQuestionController.java
@@ -2,6 +2,7 @@ package com.onexshield.wmm.controller;
 
 import com.onexshield.wmm.model.securityQuestion;
 import com.onexshield.wmm.service.securityQuestionInitializer;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,12 @@ import java.util.List;
 @Tag(name = "Question")
 public class securityQuestionController {
     private final securityQuestionInitializer securityQuestionInitializer;
+    @Operation(
+            summary = "Security questions",
+            description = "This endpoint returns all the security answers available in our api<br>" +
+                    "It doesn't require any request body or params<br>" +
+                    "It doesn't require authentication"
+    )
     @GetMapping
     public List<securityQuestion> getAll(){
         return securityQuestionInitializer.getAll();
