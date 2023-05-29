7fb2c668a69490353e9423b7233c9fdcb460e5aa
Achraf Ait Ibba <97747197+achrafaitibba@users.noreply.github.com>
2023-05-29 18:01:15 +0000

    pushing the whole project with all dependencies and packages....

---
+++ src/test/java/com/onexshield/wmm/WhereIsMyMoneyV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onexshield.wmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereIsMyMoneyV2ApplicationTests {
+	@Test
+	void contextLoads() {
+
+	}
+
+}
